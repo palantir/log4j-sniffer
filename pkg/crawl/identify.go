56ec65cb38838ee1b25b9cdd0292641e7fba9e22
Brad Moylan <bmoylan@palantir.com>
2021-12-17 14:48:43 -0800

    remove md5

--- pkg/crawl/identify.go
+++ pkg/crawl/identify.go
@@ -16,8 +16,6 @@ package crawl
 
 import (
 	"context"
-	"crypto/md5"
-	"fmt"
 	"io"
 	"io/fs"
 	"path/filepath"
@@ -36,7 +34,6 @@ const (
 	JarName                      = 1 << iota
 	JarNameInsideArchive         = 1 << iota
 	ClassPackageAndName          = 1 << iota
-	ClassFileMD5                 = 1 << iota
 )
 
 const (
@@ -47,20 +44,6 @@ var (
 	log4jRegex    = regexp.MustCompile(`(?i)^log4j-core-(\d+\.\d+(?:\..*)?)\.jar$`)
 	zipExtensions = map[string]struct{}{
 		".ear": {}, ".jar": {}, ".par": {}, ".war": {}, ".zip": {},
-	}
-	badMD5s = map[string]string{
-		// JndiManager.class (source: https://github.com/nccgroup/Cyber-Defence/blob/master/Intelligence/CVE-2021-44228/modified-classes/md5sum.txt)
-		"04fdd701809d17465c17c7e603b1b202": "log4j 2.9.0 - 2.11.2",
-		"21f055b62c15453f0d7970a9d994cab7": "log4j 2.13.0 - 2.13.3",
-		"3bd9f41b89ce4fe8ccbf73e43195a5ce": "log4j 2.6 - 2.6.2",
-		"415c13e7c8505fb056d540eac29b72fa": "log4j 2.7 - 2.8.1",
-		"5824711d6c68162eb535cc4dbf7485d3": "log4j 2.12.0 - 2.12.1",
-		"6b15f42c333ac39abacfeeeb18852a44": "log4j 2.1 - 2.3",
-		"8b2260b1cce64144f6310876f94b1638": "log4j 2.4 - 2.5",
-		"a193703904a3f18fb3c90a877eb5c8a7": "log4j 2.8.2",
-		"f1d630c48928096a484e4b95ccb162a0": "log4j 2.14.0 - 2.14.1",
-		// 2.15.0 vulnerable to Denial of Service attack (source: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-45046)
-		"5d253e53fa993e122ff012221aa49ec3": "log4j 2.15.0",
 	}
 )
 
@@ -161,9 +144,6 @@ func lookForMatchInFile(ctx context.Context, path string, size int64, contents i
 	if filename == "JndiLookup.class" {
 		return ClassName, UnknownVersion, nil
 	}
-	if version, match := fileMD5MatchesVulnerableLog4jVersion(contents); match {
-		return ClassFileMD5, version, nil
-	}
 	return NothingDetected, UnknownVersion, nil
 }
 
@@ -173,16 +153,6 @@ func fileNameMatchesVulnerableLog4jVersion(filename string) (string, bool) {
 		return "", false
 	}
 	return matches[1], true
-}
-
-func fileMD5MatchesVulnerableLog4jVersion(contents io.Reader) (string, bool) {
-	sum := md5.New()
-	if _, err := io.Copy(sum, contents); err != nil {
-		return "", false
-	}
-	hash := fmt.Sprintf("%x", sum.Sum(nil))
-	version, matches := badMD5s[hash]
-	return version, matches
 }
 
 func hasZipFileEnding(name string) bool {
