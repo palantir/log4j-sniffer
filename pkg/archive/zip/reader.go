35879b30ed90e11b2a0f4fdc93a6fc230bd80fb3
Glyn Owen Hanmer <ghanmer@palantir.com>
2022-01-21 10:51:46 +0000

    License tidying

--- pkg/archive/zip/reader.go
+++ pkg/archive/zip/reader.go
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 //
 // Copyright (c) 2022 Palantir Technologies. All rights reserved.
-//
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
